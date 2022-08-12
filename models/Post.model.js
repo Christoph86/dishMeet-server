ae705be6ec410f8d4cbac3f16f2b3d6abd37bd88
Christoph86 <ChrstphBch@web.de>
2022-08-12 10:39:15 +0200

    changed post.model to just keep name of user

--- models/Post.model.js
+++ models/Post.model.js
@@ -4,7 +4,7 @@ const { Schema, model } = mongoose;
 const postSchema = new Schema({
   title: String,
   description: String,
-  user: { type: Schema.Types.ObjectId, ref: 'User' }, //or "owner/author"
+  user: String,
 },{ timestamps: true }); //to show date of creation
 
 module.exports = model('Post', postSchema);
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -6,14 +6,12 @@ const recipeSchema = new Schema({
   description: String,
   image: String,
   servings: Number,
-  ingredients:[String],
+  ingredients:String,
   cookingAdvice: String,
   user: { type: Schema.Types.ObjectId, ref: 'User' }, //or "owner/author"
   posts: [{ type: Schema.Types.ObjectId, ref: 'Post' }], //comments to the recipe by other users
   likes: [{ type: Schema.Types.ObjectId, ref: 'User' }], 
-          //users who liked this recipe (to avoid a user gives 100 likes)
 
-  //-->bookmarks will be in the user, here no sense
 
 },{ timestamps: true }); 
 
